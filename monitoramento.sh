8c7dd3bdee2b3a97a3016505283415b19eb83995
Bruna Hidalgo <114705936+hidalgobru@users.noreply.github.com>
2025-02-27 11:01:16 -0300

    corrgingo monitoramento.sh

--- monitoramento.sh
+++ monitoramento.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo curl --silent http://ip_da_maquina > /var/log/requisicoes.log
+sudo curl --silent localhost > /var/log/requisicoes.log
 
-webhook="https://discord.com/api/webhooks/1342203545050415105/qTVPmtLsruwOGEF_by6WHL2rBfOwjVoQsg-o3bbSJeB7LlGhr6hcMtzHfWH9UgJwtBzT"
+webhook="url_webhook"
 
 data=`sudo date +%d/%m/%Y" - "%H:%M:%S`
 
